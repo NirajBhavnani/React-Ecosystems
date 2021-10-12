d3432b7ef9797f19056af4f94c6ce2e5c711b1ee
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-10-12 19:23:18 +0530

    Setting up the react project with dependencies and scripts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { hot } from 'react-hot-loader';
+
+// Functional component
+const App = () =>(
+    <div className="App">
+        <h1>Hello world!!</h1>
+    </div>
+);
+
+export default hot(module)(App);
