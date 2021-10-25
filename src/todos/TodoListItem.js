653713693271d2b210f11e0986317d63ad983cb8
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-10-25 17:29:26 +0530

    MarkTodoAsCompleted functionality added

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,14 +1,26 @@
 import React from "react";
-import './TodoListItem.css';
+import "./TodoListItem.css";
 
-const TodoListItem = ({ todo, onRemovePressed }) => (
-    <div className="todo-item-container">
-        <h3>{todo.text}</h3>
-        <div className="buttons-container">
-            <button className="completed-button">Mark as completed</button>
-            <button className="remove-button" onClick={() => onRemovePressed(todo.text)}>Remove</button>
-        </div>
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
+  <div className="todo-item-container">
+    <h3>{todo.text}</h3>
+    <div className="buttons-container">
+      {todo.isCompleted ? null : (
+        <button
+          onClick={() => onCompletedPressed(todo.text)}
+          className="completed-button"
+        >
+          Mark As Completed
+        </button>
+      )}
+      <button
+        className="remove-button"
+        onClick={() => onRemovePressed(todo.text)}
+      >
+        Remove
+      </button>
     </div>
+  </div>
 );
 
 export default TodoListItem;
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,4 +1,4 @@
-import { CREATE_TODO, REMOVE_TODO } from "./actions";
+import { CREATE_TODO, REMOVE_TODO, MARK_TODO_AS_COMPLETED } from "./actions";
 
 // reducers take 2 arguments: state, action
 export const todos = (state=[], action) => {
@@ -17,6 +17,15 @@ export const todos = (state=[], action) => {
         case REMOVE_TODO:{
             const { text } = payload;
             return state.filter(todo => todo.text !== text);
+        }
+        case MARK_TODO_AS_COMPLETED:{
+            const { text } = payload;
+            return state.map(todo =>{
+                if(todo.text === text){
+                    return {...todo, isCompleted: true};
+                }
+                return todo;
+            });
         }
         // we always have to return the unchanged state, if our reducer doesn't make any changes
         // otherwise to redux it'll look like reducer is returning undefined and it will throw an error
