a41b00288c302d15a85c331d67fb926c3c4e9bfa
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-11-08 00:33:27 +0530

    Async thunks with TodosAPI

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import TodoListItem from './TodoListItem';
-import NewTodoForm from './NewTodoForm';
-import { removeTodo, markTodoAsCompleted } from './actions';
-import './TodoList.css';
+import TodoListItem from "./TodoListItem";
+import NewTodoForm from "./NewTodoForm";
+import { removeTodo, markTodoAsCompleted } from "./actions";
+import { loadTodos } from "./thunks";
+import "./TodoList.css";
 
-const TodoList = ({ todos=[], onRemovePressed, onCompletedPressed }) => (
+const TodoList = ({
+  todos = [],
+  onRemovePressed,
+  onCompletedPressed,
+  isLoading,
+  startLoadingTodos,
+}) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, []);
+
+  const loadingMessage = <div>Loading todos...</div>;
+  const content = (
     <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
+      <NewTodoForm />
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletedPressed={onCompletedPressed}
+        />
+      ))}
     </div>
-);
+  );
+  return isLoading ? loadingMessage : content;
+};
 
-const mapStateToProps = state => ({
-    todos: state.todos,
+const mapStateToProps = (state) => ({
+  isLoading: state.isLoading,
+  todos: state.todos,
 });
 
-const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompletedPressed: text => dispatch(markTodoAsCompleted(text))
+const mapDispatchToProps = (dispatch) => ({
+  startLoadingTodos: () => dispatch(loadTodos()),
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,36 +1,57 @@
-import { CREATE_TODO, REMOVE_TODO, MARK_TODO_AS_COMPLETED } from "./actions";
+import {
+  CREATE_TODO,
+  REMOVE_TODO,
+  MARK_TODO_AS_COMPLETED,
+  LOAD_TODOS_IN_PROGRESS,
+  LOAD_TODOS_SUCCESS,
+  LOAD_TODOS_FAILURE,
+} from "./actions";
 
-// reducers take 2 arguments: state, action
-export const todos = (state=[], action) => {
-    const { type, payload } = action;
-    switch(type){
-        case CREATE_TODO:{
-            // fetching the text property from CREATE_TODO payload
-            const { text } = payload;
-            // new state: newTodo
-            const newTodo = {
-                text,
-                isCompleted: false,
-            };
-            return state.concat(newTodo); //returning new state
-        }
-        case REMOVE_TODO:{
-            const { text } = payload;
-            return state.filter(todo => todo.text !== text);
-        }
-        case MARK_TODO_AS_COMPLETED:{
-            const { text } = payload;
-            return state.map(todo =>{
-                if(todo.text === text){
-                    return {...todo, isCompleted: true};
-                }
-                return todo;
-            });
-        }
-        // we always have to return the unchanged state, if our reducer doesn't make any changes
-        // otherwise to redux it'll look like reducer is returning undefined and it will throw an error
-        default:{
-            return state;
-        }
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+
+    switch (type) {
+    case LOAD_TODOS_IN_PROGRESS:
+        return true;
+    case LOAD_TODOS_SUCCESS:
+    case LOAD_TODOS_FAILURE:
+        return false;
+    default:
+        return state;
     }
 }
+
+// reducers take 2 arguments: state, action
+export const todos = (state = [], action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case CREATE_TODO: {
+      // fetching the text property from CREATE_TODO payload
+      const { text } = payload;
+      // new state: newTodo
+      const newTodo = {
+        text,
+        isCompleted: false,
+      };
+      return state.concat(newTodo); //returning new state
+    }
+    case REMOVE_TODO: {
+      const { text } = payload;
+      return state.filter((todo) => todo.text !== text);
+    }
+    case MARK_TODO_AS_COMPLETED: {
+      const { text } = payload;
+      return state.map((todo) => {
+        if (todo.text === text) {
+          return { ...todo, isCompleted: true };
+        }
+        return todo;
+      });
+    }
+    // we always have to return the unchanged state, if our reducer doesn't make any changes
+    // otherwise to redux it'll look like reducer is returning undefined and it will throw an error
+    default: {
+      return state;
+    }
+  }
+};
